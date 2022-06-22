d491b8d39e6a46103a9516553375b5d01d8463ed
Hoangvu518 <Hoangvu518@gmail.com>
2022-06-22 16:14:03 -0700

    hello

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -7,29 +7,7 @@ namespace ConsoleApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World 2!");
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World 2!");
-
-            Console.WriteLine("Hello World 2!");
-            Console.WriteLine("Hello World 2!");
-            Console.WriteLine("Hello World 2!");
-            Console.WriteLine("Hello World 2!");
-            Console.WriteLine("Hello World 2!");
-            Console.WriteLine("Hello World 2!");
-
-
-            Console.WriteLine("Hello World 2!");
-            Console.WriteLine("Hello World 2!");
-            Console.WriteLine("Hello World 2!");
-            Console.WriteLine("Hello World 2!");
-            Console.WriteLine("Hello World 2!");
-            Console.WriteLine("Hello World 2!");
-            Console.WriteLine("Hello World 2!");
-            Console.WriteLine("Hello World 2!");
-            Console.WriteLine("Hello World 2!");
-            Console.WriteLine("Hello World 2!");
-            Console.WriteLine("Hello World 2!");
+            //refactored the code.
 
         }
     }
